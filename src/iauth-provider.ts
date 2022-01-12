af76d703ad5f84dd9cd92d1a1066901d3d19c7d5
Kenneth Tan Xin You <kennethtxytqw@users.noreply.github.com>
2022-01-12 17:08:31 +0800

    Added IAsyncAuthProvider

--- src/iauth-provider.ts
+++ src/iauth-provider.ts
@@ -3,3 +3,9 @@ export interface IAuthProvider {
 
     getApiKey(): string;
 }
+
+export interface IAsyncAuthProvider {
+    async signJwt(path: string, bodyJson?: any): Promise<string>;
+
+    getApiKey(): string;
+}
